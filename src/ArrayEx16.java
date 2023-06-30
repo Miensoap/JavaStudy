a5c97bd703baecbaf2bc014a7a01330b85685734
Miensoap <miensop51@gmail.com>
2023-06-30 16:49:33 +0900

    230630

---
+++ src/ArrayEx16.java
@@ -0,0 +1,8 @@
+public class ArrayEx16 {
+    public static void main(String[] args) {
+        System.out.println("args.length = " + args.length);
+        for(int i=0;i<args.length;i++){
+            System.out.println("args = " + args[i]);
+        }
+    }
+}
