21e47b80bff0babd9f6e1a5e13cc843c508eff92
Miensoap <miensop51@gmail.com>
2023-08-14 12:52:53 +0900

    230814

--- src/baseball/Application.java
+++
@@ -1,9 +0,0 @@
-package baseball;
-
-
-public class Application {
-    public static void main(String[] args) {
-        GameLauncher baseballgame = new GameLauncher();
-        baseballgame.run();
-    }
-}
