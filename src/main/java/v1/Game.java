d1582230e8309a5ccafd6efe447cf9d87fe732e6
Miensoap <miensop51@gmail.com>
2023-11-20 15:23:32 +0900

    feat : add reGame

--- src/main/java/v1/Game.java
+++ src/main/java/v1/Game.java
@@ -1,21 +1,22 @@
 package v1;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Game {
-    int round,win,lose;
+    int round=1;
+    int win,lose=0;
     static User player = new User();
     static User dealer = new User();
     Random random = new Random();
-    public Game(int round){
-        this.round = round;
-    }
     public void run(){
         System.out.println("Game " + round);
         draw();
         System.out.println("You   : ["+player.getCardNumber()+"]");
         System.out.println("Dealer: ["+dealer.getCardNumber()+"]");
         judge();
+        System.out.println("현재 전적: "+win+"승 "+lose+"패");
+        reGame();
     }
 
     private void draw(){
@@ -25,13 +26,32 @@ public class Game {
 
     private void judge(){
         if (player.getCardNumber()>dealer.getCardNumber()){
+            this.win+=1;
             System.out.println("당신이 이겼습니다.");
         }
         else if (player.getCardNumber()<dealer.getCardNumber()){
             System.out.println("딜러가 이겼습니다.");
+            this.lose+=1;
         }
         else{
             System.out.println("비겼습니다.");
+        }
+    }
+
+    private void reGame(){
+        System.out.print("한 게임 더 하시겠습니까? (Y / N) ");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        try{
+            if (input .equals("Y")) run();
+            else if (input.equals("N"));
+            else {
+                Exception e = new Exception("잘못입력");
+                throw e;
+            }
+        }catch (Exception e1) {
+            System.out.println("잘못 입력하셨습니다.");
+            reGame();
         }
     }
 
