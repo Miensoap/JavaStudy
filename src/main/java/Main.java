7275d4fddbf849f52224dbd924e1ad563fe942f3
miensoap2 <minsurpark@gmail.com>
2024-07-08 21:57:28 +0900

    중복 제거해 저장하는 로직 추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,17 @@
 import java.io.BufferedWriter;
+import java.io.BufferedReader;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.StringJoiner;
+import java.util.stream.Collectors;
 
 public class Main {
     private static final String filePath = "./result.txt";
@@ -14,8 +20,18 @@ public class Main {
         NaverNewsCrawler naverNewsCrawler = new NaverNewsCrawler();
 
         List<Article> allArticles = naverNewsCrawler.getAllArticles();
-        StringJoiner content = new StringJoiner("\n,");
-        allArticles.forEach(article -> content.add(article.toString()));
+        Set<String> existingLinks = getExistingLinks();
+
+        List<Article> newArticles = allArticles.stream()
+            .filter(article -> !existingLinks.contains(article.link()))
+            .toList();
+
+        writeResult(newArticles);
+    }
+
+    private static void writeResult(List<Article> articles) {
+        StringJoiner content = new StringJoiner("\n");
+        articles.forEach(article -> content.add(article.toString()));
 
         try (Writer writer = new BufferedWriter(new OutputStreamWriter(
                 new FileOutputStream(filePath), StandardCharsets.UTF_8))) {
@@ -23,5 +39,18 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static Set<String> getExistingLinks() {
+        Set<String> existingLinks = new HashSet<>();
+
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(filePath), StandardCharsets.UTF_8)) {
+            existingLinks = reader.lines()
+                    .map(line -> line.substring(line.lastIndexOf("(") + 1, line.lastIndexOf(")")))
+                    .collect(Collectors.toSet());
+        } catch (IOException e) {
+            // 파일이 없으면 중복 없음
+        }
+        return existingLinks;
     }
 }
