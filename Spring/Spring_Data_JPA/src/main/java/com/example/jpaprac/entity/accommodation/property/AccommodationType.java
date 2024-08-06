332597429bb44adcf2e2c9c0b630cab444365fd8
miensoap <miensop51@gmail.com>
2024-08-07 08:02:07 +0900

    restore

--- Spring/Spring_Data_JPA/src/main/java/com/example/jpaprac/entity/accommodation/property/AccommodationType.java
+++
@@ -1,5 +0,0 @@
-package com.example.jpaprac.entity.accommodation.property;
-
-public enum AccommodationType {
-    APARTMENT, HOTEL, MOTEL, HOSTEL, GUEST_HOUSE, RESORT, BED_AND_BREAKFAST, VACATION_RENTAL, COTTAGE, CABIN, CAMPGROUND
-}
--- Spring/Spring_Data_JPA/src/main/java/com/example/jpaprac/repository/ReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.jpaprac.repository;
-
-import com.example.jpaprac.entity.ReviewEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReviewRepository extends JpaRepository<ReviewEntity, Long> {
-}
